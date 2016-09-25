f8a14d320a9a6ee0a0c08647478aba703b96ef91
DIMNAT <dim_nat@list.ru>
2016-09-25 22:38:39 +0300

    Осваиваем HorizontalLine

---
+++ Snake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(2, 4, '#');
+            p2.Draw();
+
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '$');
+            line.Drow();
+
+            VerticalLine line2 = new VerticalLine(12, 5, 11, '&');
+            line2.Drow();
+
+           
+
+            Console.ReadLine();
+        } // конец Main
+
+       
+
+    } // конец Program
+}
---
+++ Snake/Snake/RAM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Point p3 = new Point(8, 3, '^^^');
+            p3.Draw();
+
+            Point p4 = new Point(1, 8, '$$$___$$$');
+            p4.Draw();
+
+            Console.ReadLine();
+        } // конец Main
+
+       
+
+    } // конец Program
+}
