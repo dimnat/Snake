d7253db24283c0cb1fd8b02c62fc8aa89c07284e
DIMNAT <dim_nat@list.ru>
2016-09-25 16:05:22 +0300

    осваиваем класс List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,63 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
+
             Point p1 = new Point(1, 3, '*');
-         
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + "p1.y = " + p1.y);
 
             Point p2 = new Point(4, 5, '#');
-          
-            p2.Draw();
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + "p1.y = " + p1.y+"; p2.x = " + p2.x + "p2.y = " + p2.y);
+
+            Point p3 = new Point(1, 3, '*');
+            Update(p3);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + "p1.y = " + p1.y);
+
+            Point p4 = new Point(4, 5, '#');
+            Reset(p4);
 
             Console.ReadLine();
         } // конец Main
+
+        public static void Func1(int value)
+        {
+            
+        }
+
+        public static void Func2(int value)
+        {
+            value = value + 2;
+        }
+
+        public static void Func3(int x)
+        {
+            x = x + 1;
+        }
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Reset(Point p, int dx, int dy)
+        {
+            p = new Point();
+        }
 
     } // конец Program
 }
